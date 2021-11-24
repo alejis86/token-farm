601aadeb634471c331dc9e11f53cbc2955dbfd3e
Mayra <mayra@iMac-de-Mayra.local>
2021-11-24 09:06:25 -0800

    Updates

---
+++ precache-manifest.2a9297b39d8c20daae278c233d43d433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a26eab680950dc03d12d",
+    "url": "/static/css/main.5a8803ad.chunk.css"
+  },
+  {
+    "revision": "a26eab680950dc03d12d",
+    "url": "/static/js/main.a26eab68.chunk.js"
+  },
+  {
+    "revision": "3ca1ed1c66473fc627c1",
+    "url": "/static/css/1.61f79be0.chunk.css"
+  },
+  {
+    "revision": "3ca1ed1c66473fc627c1",
+    "url": "/static/js/1.3ca1ed1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "637533c199cc83589865e8c36fa1faf0",
+    "url": "/index.html"
+  }
+];
